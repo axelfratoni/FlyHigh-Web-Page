945379f67200402584da71cca37c5645eed8480d
Axel <afratoni@itba.edu.ar>
2017-05-07 17:53:48 -0300

    Mas comportamiento

--- bootstrap-3.3.7-dist/js/myjs.js
+++ bootstrap-3.3.7-dist/js/myjs.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$("#spinUp").on("click",function(){
+	$("#spinUp").click(function(){
 		var target = $(this).data("target");
 		var val = parseInt(document.getElementById(target).value) + 1;
 		if (val > 4){
@@ -10,7 +10,7 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-	$("#spinDown").on("click",function(){
+	$("#spinDown").click(function(){
 		var target = $(this).data("target");
 		var val = parseInt(document.getElementById(target).value) - 1;
 		if (val < 0){
@@ -18,4 +18,16 @@ $(document).ready(function(){
 		}
 		document.getElementById(target).value=val ; 
 	});	
-})
+});
+
+$(document).ready(function(){
+	$("#idaYvuelta").change(function(){
+		$( "#fechas" ).append($('<div class="form-group row" id="fechaLlegada"><label class="col-md-3 col-form-label" for="arrivalDate"><h5>Fecha de Llegada:</h5></label><div class="col-md-4"><input class="form-control" type="date" id="arrivalDate" placeholder="Ingrese su fecha de llegada" ></div></div>').hide().fadeIn(1000));
+	});	
+});
+
+$(document).ready(function(){
+	$("#ida").change(function(){
+		$( "#fechaLlegada" ).fadeOut(function() {$(this).remove();});
+	});	
+});

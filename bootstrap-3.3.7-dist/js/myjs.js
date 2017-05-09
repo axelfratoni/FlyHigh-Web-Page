ac079ae060769b4d6ea129dbad5d8cf69ae01d37
Axel <afratoni@itba.edu.ar>
2017-05-09 11:13:06 -0300

    Agregado de promociones

--- bootstrap-3.3.7-dist/js/myjs.js
+++ bootstrap-3.3.7-dist/js/myjs.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$("#spinUp").click(function(){
+	$('.spinUp').click(function(){
 		var target = $(this).data("target");
 		var val = parseInt(document.getElementById(target).value) + 1;
 		if (val > 4){
@@ -10,7 +10,7 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-	$("#spinDown").click(function(){
+	$('.spinDown').click(function(){
 		var target = $(this).data("target");
 		var val = parseInt(document.getElementById(target).value) - 1;
 		if (val < 0){
@@ -30,4 +30,21 @@ $(document).ready(function(){
 	$("#ida").change(function(){
 		$( "#fechaLlegada" ).fadeOut(function() {$(this).remove();});
 	});	
+});
+
+$(function() {
+  // This will select everything with the class smoothScroll
+  // This should prevent problems with carousel, scrollspy, etc...
+  $('.smoothScroll').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000); // The number here represents the speed of the scroll in milliseconds
+        return false;
+      }
+    }
+  });
 });

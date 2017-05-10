18c3a074e844713cc4a9df7d272e50b26532b05d
gastonrod <grodriguez@itba.edu.ar>
2017-05-10 12:19:13 -0300

    Agregado menu de filtros en la pagina de eleccion. Arreglado el menu dropdown de tipo de documento que cambiaba de valor. Agregadas fotos de los prototipos

--- bootstrap-3.3.7-dist/js/myjs.js
+++ bootstrap-3.3.7-dist/js/myjs.js
@@ -1,12 +1,19 @@
+$(function(){
+	 $(".dropdown-menu li a").click(function(){
+		 $("#documentDropDown").text($(this).text());
+		 $("#documentDropDown").val($(this).text());
+	});
+});
+
 $(document).ready(function(){
 	$('.spinUp').click(function(){
 		var target = $(this).data("target");
 		var val = parseInt(document.getElementById(target).value) + 1;
 		if (val > 4){
-			val = 0
+			val = 4
 		}
-		document.getElementById(target).value=val ; 
-	});	
+		document.getElementById(target).value=val ;
+	});
 });
 
 $(document).ready(function(){
@@ -14,22 +21,22 @@ $(document).ready(function(){
 		var target = $(this).data("target");
 		var val = parseInt(document.getElementById(target).value) - 1;
 		if (val < 0){
-			val = 4
+			val = 0
 		}
-		document.getElementById(target).value=val ; 
-	});	
+		document.getElementById(target).value=val ;
+	});
 });
 
 $(document).ready(function(){
 	$("#idaYvuelta").change(function(){
 		$( "#fechas" ).append($('<div class="form-group row" id="fechaLlegada"><label class="col-md-3 col-form-label" for="arrivalDate"><h5>Fecha de Llegada:</h5></label><div class="col-md-4"><input class="form-control" type="date" id="arrivalDate" placeholder="Ingrese su fecha de llegada" ></div></div>').hide().fadeIn(1000));
-	});	
+	});
 });
 
 $(document).ready(function(){
 	$("#ida").change(function(){
 		$( "#fechaLlegada" ).fadeOut(function() {$(this).remove();});
-	});	
+	});
 });
 
 $(function() {
